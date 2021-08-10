a79f4054a5191e5ed7e4a407a886ed45049b0ef4
viniciussti <viniciussantos8@hotmail.com>
2021-08-10 16:58:08 -0300

    Consertando bugs na postagem

--- src/app/entrar/entrar.component.ts
+++ src/app/entrar/entrar.component.ts
@@ -27,6 +27,7 @@ export class EntrarComponent implements OnInit {
       environment.token = this.userLogin.token
       environment.nome = this.userLogin.nome
       environment.foto = this.userLogin.foto
+      environment.id = this.userLogin.id
 
       this.router.navigate(['/inicio'])
 
